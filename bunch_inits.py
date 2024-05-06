2445eba4565c3f25c18e71f265387c572f58dc07
Ben Folsom <benfolsom@gmail.com>
2024-05-06 16:42:08 +0200

    stable 50 micron 3TeV 1 meter with two-phase stepsize

--- bunch_inits.py
+++ bunch_inits.py
@@ -32,6 +32,10 @@ def init_bunch(starting_distance,transv_mom,starting_Pz,stripped_ions,
     bdoty = np.zeros(pcount)#by*np.random.uniform(-8e-2,8e-2) 
     bdotz = np.zeros(pcount)#bz*np.random.uniform(-8e-2,8e-2)
 
+    #bdotx = bx*np.random.uniform(-8e-2,8e-2) 
+    #bdoty = by*np.random.uniform(-8e-2,8e-2) 
+    #bdotz = bz*np.random.uniform(-8e-2,8e-2)
+
     init_bunch_dict = {'x':x, 'y':y, 'z':z, 't':t, 'Px':Px, 'Py':Py, 'Pz':Pz,'Pt':Pt,
                 'bx':bx,'by':by,'bz':bz,'bdotx':bdotx,'bdoty':bdoty,'bdotz':bdotz,'gamma':gamma,'q':q,'m':mass,'char_time':char_time}
                 
