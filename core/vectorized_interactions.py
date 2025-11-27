c3c0320221c045906e6414e59ccac242bc669230
Ben Folsom <benfolsom@gmail.com>
2025-11-27 23:41:40 +0100

    using external particle charge for accumulated scalar pot

--- core/vectorized_interactions.py
+++ core/vectorized_interactions.py
@@ -245,7 +245,7 @@ def compute_vectorized_contributions(
 
     # Compute scalar potential sum: Σ(q_j / (R_sep_j * k_factor_j))
     # This is used for correct gamma calculation: γ = (Pt - q²·Φ) / (mc)
-    scalar_potential_sum = float(np.sum(charge_ext / (R_sep * k_factor)))
+    scalar_potential_sum = float(np.sum(charge_ext**2 / (R_sep * k_factor)))
 
     return (
         delta_px,
