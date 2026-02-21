d7f6a0ffee65e43688e596bddb99d193ead2ca9c
Ben Folsom <benjamin.folsom@maxiv.lu.se>
2026-02-21 21:49:39 +0100

    Bump version to 0.4.12

--- core/_version.py
+++ core/_version.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 __all__ = ["__version__", "VERSION"]
 
 # Update this string when releasing a new version.
-__version__ = "0.4.11"
+__version__ = "0.4.12"
 
 # Alias to make semantic version access explicit.
 VERSION = __version__
