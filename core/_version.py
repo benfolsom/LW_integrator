c444e53b6bb76141fbab1a4cdc183c5693d1cb46
Benjamin Folsom <benfolsom@gmail.com>
2026-04-22 10:32:15 +0200

    Bump version to 0.6.7

--- core/_version.py
+++ core/_version.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 __all__ = ["__version__", "VERSION"]
 
 # Update this string when releasing a new version.
-__version__ = "0.6.6"
+__version__ = "0.6.7"
 
 # Alias to make semantic version access explicit.
 VERSION = __version__
